03ecf4f9bbfe7a38e1cdc37e4b60b3e0c1e65281
Rohan Singh <ff3870422@gmail.com>
2025-10-21 23:04:31 +0530

    Adding template and wrap up

--- consumer.go
+++ consumer.go
@@ -2,12 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/smtp"
 	"sync"
+	"time"
 )
 
-func emailWorker(id int, ch chan Receiver, wg *sync.WaitGroup){
+func emailWorker(id int, ch chan Receiver, wg *sync.WaitGroup) {
 	defer wg.Done()
+
+	smtpHost := "localhost"
+	smtpPort := "1025"
 	for receiver := range ch {
-		fmt.Println(id, receiver)
+		// formattedMsg := fmt.Sprintf("To: %s\r\nSubject: Its for %s\r\n\r\n%s", receiver.Email, receiver.Name, "Just an campaign email")
+		formattedMsg, err := execTemplate(receiver)
+		if err != nil {
+			// TODO: Add proper error handling  logging
+			log.Printf("Worker %d, Facing issue with email %s", id, receiver.Email)
+			continue
+		}
+		msg := []byte(formattedMsg)
+		fmt.Printf("Worker %d, Sending email to %s", id, receiver.Email)
+
+		err = smtp.SendMail(smtpHost+":"+smtpPort, nil, "rohan@singh.com", []string{receiver.Email}, msg)
+		if err != nil {
+			log.Fatal("Error sending email!", err.Error())
+		}
+		// Delay to prevent from SMTP Server rate limiting
+		time.Sleep(50 * time.Millisecond)
+
+		fmt.Printf("Worker %d, Send email to %s", id, receiver.Email)
 	}
 }
--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "sync"
+import (
+	"bytes"
+	"html/template"
+	"sync"
+)
 
 func main() {
 	ch := make(chan Receiver)
@@ -16,4 +20,19 @@ func main() {
 	}
 
 	wg.Wait()
+}
+
+func execTemplate(r Receiver) (string, error) {
+	t, err := template.ParseFiles("email.tmpl")
+	if err != nil {
+		return "", err
+	}
+
+	var tpl bytes.Buffer
+	err = t.Execute(&tpl, r)
+	if err != nil{
+		return "", err
+	}
+
+	return tpl.String(), nil
 }
